c54a6813fc5dac2f7bf11bdddd8a9e8a1a3c886a
MakJovanovic <mak.jovanovic@stu.ibu.edu.ba>
2025-12-27 21:59:32 +0100

    Pokusaj6

--- Frontend/Utils/constants.js
+++ Frontend/Utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-    PROJECT_BASE_URL: location.hostname == "localhost" ? "http://localhost/Gotovo5/Backend/" : "https://ultimategoalkeepingsite-ft49w.ondigitalocean.app/assignment-1-web-programming--backend/",
+    PROJECT_BASE_URL: location.hostname == "localhost" ? "http://localhost/Gotovo5/Backend/" : "https://ultimategoalkeepingsite-ft49w.ondigitalocean.app/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
  }
