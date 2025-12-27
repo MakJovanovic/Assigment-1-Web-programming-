809af79de6d62cb82cbc9bba2164133516abb22b
MakJovanovic <mak.jovanovic@stu.ibu.edu.ba>
2025-12-27 19:09:11 +0100

    Pokusaj2

--- Frontend/Utils/constants.js
+++ Frontend/Utils/constants.js
@@ -1,5 +1,5 @@
 let Constants = {
-    PROJECT_BASE_URL: "http://localhost/Gotovo5/Backend/",
+    PROJECT_BASE_URL: location.hostname == "localhost" ? "http://localhost/Gotovo5/Backend/" : "https://assigment-1-web-programming--bac/",
     USER_ROLE: "user",
     ADMIN_ROLE: "admin"
  }
